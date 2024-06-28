471e3fef2413bbdb848fa47bffbbedc8e7550677
Luka Camus <luka.camus@epitech.eu>
2024-06-28 11:26:20 +0200

    feat: add phoenix framework to elixir
    for backend development

---
+++ lib/back_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule BackWeb.PageController do
+  use BackWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
